dd717f9440fcd82c5a066c51dc019e835497660f
pablocn3 <138717495+pablocn3@users.noreply.github.com>
2023-07-19 15:09:34 -0400

    Update App.js

--- apidsa/src/App.js
+++ apidsa/src/App.js
@@ -5,65 +5,7 @@ function App() {
     <div className="App">
 
 
-    <Card
-    IDS='1'
-     />
-     
-    <Card
-    id='2'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-
-    <Card
-    id='3'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-       
-    <Card
-    id='4'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-
-    <Card
-    id='5'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-
-    <Card
-    id='6'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-
-      
-    <Card
-    id='7'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-
-    <Card
-    id='8'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-
-    <Card
-    id='9'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-       
-    <Card
-    id='10'
-     alt='Imagen 1'
-     titulo='Titulo de mi Card'
-     parrafo='Parrafo de el Card'/>
-       
+    <Card/>
 
       
     </div>
